1b29e7fca3b0ae05ac76dcc591d8edb56e89da46
cos-enohara <ftndskman@yahoo.co.jp>
2020-11-18 15:35:52 +0900

    a

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { AmazonAIConvertPredictionsProvider } from '@aws-amplify/predictions';
 
 const initialFormState = { name: '', description: '' }
 
@@ -89,5 +90,5 @@ function App() {
     </div>
   );
 }
-
+// gitaaa
 export default withAuthenticator(App);
